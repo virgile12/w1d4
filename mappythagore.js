77ebc0b7608d5d367ea23fb60392c6a490c09e61
virg12 <virgile_patrignani@hotmail.com>
2019-03-07 18:15:06 +0000

    solution to mappythargore.js

---
+++ mappythagore.js
@@ -0,0 +1,14 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(z) {
+
+ return Math.sqrt((Math.pow(z.x, 2) + Math.pow(z.y, 2)))
+
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
