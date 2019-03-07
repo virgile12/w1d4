4abd742f7c6dbc855fc2a227abf848bd9c6d55a7
virg12 <virgile_patrignani@hotmail.com>
2019-03-07 17:25:43 +0000

    forEach() loop for waldo index

--- findWaldo.js
+++ findWaldo.js
@@ -1,4 +1,36 @@
 function findWaldo(arr, found) {
+
+arr.forEach( function(el, i) {
+
+    if ( el === "Waldo") {
+
+      found(i);   // execute callback
+    }
+  }
+)}
+
+function actionWhenFound(i) {
+
+  console.log("Found Waldo at index: " + i);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function findWaldo(arr, found) {
   var index = 0;
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
@@ -13,4 +45,4 @@ function actionWhenFound(i) {
   console.log("Found Waldo at index: " + i);
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound); */
